f79fceabd17c05732b4d372635b872fa675486af
菅原 清吾 <sugawara_seigo@cyberagent.co.jp>
2018-06-06 12:17:30 +0900

    making top layout

--- AMoAdMoPubDemo/app/src/main/java/amoad/com/amoadmopubdemo/MainActivity.kt
+++ AMoAdMoPubDemo/app/src/main/java/amoad/com/amoadmopubdemo/MainActivity.kt
@@ -2,11 +2,26 @@ package amoad.com.amoadmopubdemo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.amoad.AMoAdBuildConfig
+import com.amoad.AMoAdLogger
 
 class MainActivity : AppCompatActivity() {
+
+//    private val ITEMS : ArrayList<Array<Any>> = arrayOf(
+//            "Banner広告", BannerActivity::class.java
+//    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        AMoAdLogger.getInstance().setEnabled(true)
+        AMoAdBuildConfig.toStaging()
+
+        initList()
+    }
+
+    private fun initList(){
+//        ITEMS.add(arrayOf("Banner広告", BannerActivity::class.java))
     }
 }
