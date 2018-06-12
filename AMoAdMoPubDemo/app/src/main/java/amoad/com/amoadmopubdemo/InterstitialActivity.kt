b472923879bb1a6e739e974ca1e8fe874d50f762
菅原 清吾 <sugawara_seigo@cyberagent.co.jp>
2018-06-12 18:02:06 +0900

    interstitial adapter

--- AMoAdMoPubDemo/app/src/main/java/amoad/com/amoadmopubdemo/InterstitialActivity.kt
+++ AMoAdMoPubDemo/app/src/main/java/amoad/com/amoadmopubdemo/InterstitialActivity.kt
@@ -2,11 +2,51 @@ package amoad.com.amoadmopubdemo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.mopub.mobileads.MoPubErrorCode
+import com.mopub.mobileads.MoPubInterstitial
 
-class InterstitialActivity : AppCompatActivity() {
+class InterstitialActivity : AppCompatActivity(), MoPubInterstitial.InterstitialAdListener {
+
+    private var mMoPubInterstitial: MoPubInterstitial? = null
+    private var adUnitID = "4981e692297148ffa953b5506892ebc5"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_interstitial)
+
+        if (mMoPubInterstitial == null) {
+            mMoPubInterstitial = MoPubInterstitial(this, this.adUnitID)
+            mMoPubInterstitial?.setInterstitialAdListener(this@InterstitialActivity)
+        }
+        mMoPubInterstitial?.load()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (mMoPubInterstitial != null) {
+            mMoPubInterstitial?.destroy()
+            mMoPubInterstitial = null
+        }
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+    }
+
+    // InterstitialAdListener implementation
+    override fun onInterstitialLoaded(interstitial: MoPubInterstitial) {}
+
+    override fun onInterstitialFailed(interstitial: MoPubInterstitial, errorCode: MoPubErrorCode) {}
+
+    override fun onInterstitialShown(interstitial: MoPubInterstitial) {}
+
+    override fun onInterstitialClicked(interstitial: MoPubInterstitial) {}
+
+    override fun onInterstitialDismissed(interstitial: MoPubInterstitial) {}
+
+    private fun show() {
+    }
+
+    private fun load() {
     }
 }
---
+++ AMoAdMoPubDemo/app/src/main/java/com/mopub/mobileads/AMoAdMoPubAdapterInterstitial.kt
@@ -0,0 +1,18 @@
+package com.mopub.mobileads
+
+import android.content.Context
+import android.util.Log
+
+open class AMoAdMoPubAdapterInterstitial : CustomEventInterstitial() {
+
+    override fun loadInterstitial(context: Context?, customEventInterstitialListener: CustomEventInterstitialListener?, localExtras: MutableMap<String, Any>?, serverExtras: MutableMap<String, String>?) {
+    }
+
+    override fun showInterstitial() {
+
+    }
+
+    override fun onInvalidate() {
+
+    }
+}
