1031fb105c03687206c7612bbda9a056501d9b36
菅原 清吾 <sugawara_seigo@cyberagent.co.jp>
2018-06-12 14:01:06 +0900

    bump

--- AMoAdMoPubDemo/app/src/main/java/amoad/com/amoadmopubdemo/BannerActivity.kt
+++ AMoAdMoPubDemo/app/src/main/java/amoad/com/amoadmopubdemo/BannerActivity.kt
@@ -3,31 +3,30 @@ package amoad.com.amoadmopubdemo
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.ViewGroup
 import com.mopub.mobileads.MoPubErrorCode
 import com.mopub.mobileads.MoPubView
 import kotlinx.android.synthetic.main.activity_banner.*
 
 class BannerActivity : AppCompatActivity(), MoPubView.BannerAdListener {
 
-    private var moPubView: MoPubView? = null
-    var adUnitID: String? = "eeb433b2c67848fb9148c6b091d8bb42"
+    private var adUnitID = "eeb433b2c67848fb9148c6b091d8bb42"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_banner)
 
-        if (moPubView == null) {
-            moPubView = MoPubView(this)
-            moPubView?.setAdUnitId(this.adUnitID)
-            moPubView?.setBannerAdListener(this)
-            moPubView?.setBannerAdListener(this@BannerActivity)
-            moPubView?.setKeywords("")
-            moPubView?.loadAd()
-        }
+        var moPubView = MoPubView(this)
+        moPubView.setAdUnitId(this.adUnitID)
+        moPubView.setBannerAdListener(this)
+        moPubView.setBannerAdListener(this@BannerActivity)
+        moPubView.setKeywords("")
+        moPubView.loadAd()
     }
 
     override fun onBannerLoaded(moPubView: MoPubView) {
-        this.moPubView = moPubView
+        var content = banner_mopubview as ViewGroup
+        content.addView(moPubView)
     }
 
     override fun onBackPressed() {
@@ -35,7 +34,6 @@ class BannerActivity : AppCompatActivity(), MoPubView.BannerAdListener {
     }
 
     override fun onDestroy() {
-        moPubView!!.destroy()
         super.onDestroy()
     }
 
