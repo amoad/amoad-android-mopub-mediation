55e2a6f8dbd12f18f00b6d870ffa37bc8bb1c706
菅原 清吾 <sugawara_seigo@cyberagent.co.jp>
2018-06-19 16:42:05 +0900

    fix banner adapter

--- AMoAdMoPubDemo/app/src/main/java/com/mopub/mobileads/AMoAdMoPubAdapterBanner.kt
+++ AMoAdMoPubDemo/app/src/main/java/com/mopub/mobileads/AMoAdMoPubAdapterBanner.kt
@@ -5,38 +5,45 @@ import android.util.Log
 import com.amoad.AMoAdView
 import com.amoad.AdCallback
 
-open class AMoAdMoPubAdapterBanner : CustomEventBanner() {
+open class AMoAdMoPubAdapterBanner : CustomEventBanner() ,AdCallback {
+
+    private var _amoadView: AMoAdView? = null
+    private var _customEventBannerListener: CustomEventBannerListener? = null
 
     override fun loadBanner(context: Context?, customEventBannerListener: CustomEventBannerListener?, localExtras: MutableMap<String, Any>?, serverExtras: MutableMap<String, String>?) {
 
-        customEventBannerListener ?: return
+        _customEventBannerListener = customEventBannerListener
+        _customEventBannerListener ?: return
 
         var customEventClassData = AMoAdMoPubUtil.extractCustomEventClassDataForDisplay(serverExtras)
         customEventClassData ?: return
 
-        val mAdView = AMoAdView(context)
-        mAdView?.sid = customEventClassData.sid
-        mAdView?.setCallback(object : AdCallback {
-            override fun didReceiveAd() {
-                //受信成功
-                Log.d("debug", "didReceiveAd")
-                customEventBannerListener.onBannerLoaded(mAdView)
-            }
-
-            override fun didFailToReceiveAdWithError() {
-                //受信失敗
-                Log.d("debug", "didFailToReceiveAdWithError")
-                customEventBannerListener.onBannerFailed(null)
-            }
-
-            override fun didReceiveEmptyAd() {
-                //広告が配信されてない
-                Log.d("debug", "didReceiveEmptyAd")
-                customEventBannerListener.onBannerFailed(null)
-            }
-        })
+        var adView = AMoAdView(context)
+        adView?.let {
+            adView.sid = customEventClassData.sid
+            adView.setCallback(this)
+            _amoadView = adView
+        }
     }
 
     override fun onInvalidate() {
+    }
+
+    override fun didReceiveAd() {
+        //受信成功
+        Log.d("debug", "didReceiveAd")
+        _customEventBannerListener?.onBannerLoaded(_amoadView)
+    }
+
+    override fun didFailToReceiveAdWithError() {
+        //受信失敗
+        Log.d("debug", "didFailToReceiveAdWithError")
+        _customEventBannerListener?.onBannerFailed(null)
+    }
+
+    override fun didReceiveEmptyAd() {
+        //広告が配信されてない
+        Log.d("debug", "didReceiveEmptyAd")
+        _customEventBannerListener?.onBannerFailed(null)
     }
 }
