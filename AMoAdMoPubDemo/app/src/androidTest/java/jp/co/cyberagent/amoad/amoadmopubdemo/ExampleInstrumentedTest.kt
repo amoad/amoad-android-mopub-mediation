f61f339a97ed7ca418ed99cc013fa924698850dd
菅原 清吾 <sugawara_seigo@cyberagent.co.jp>
2018-06-22 15:50:32 +0900

    rename package

--- AMoAdMoPubDemo/app/src/androidTest/java/jp/co/cyberagent/amoad/amoadmopubdemo/ExampleInstrumentedTest.kt
+++ AMoAdMoPubDemo/app/src/androidTest/java/jp/co/cyberagent/amoad/amoadmopubdemo/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package amoad.com.amoadmopubdemo
+package jp.co.cyberagent.amoad.amoadmopubdemo
 
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("amoad.com.amoadmopubdemo", appContext.packageName)
+        assertEquals("jp.co.cyberagent.amoad.amoadmopubdemo", appContext.packageName)
     }
 }
--- AMoAdMoPubDemo/app/src/main/java/jp/co/cyberagent/amoad/amoadmopubdemo/InterstitialActivity.kt
+++ AMoAdMoPubDemo/app/src/main/java/jp/co/cyberagent/amoad/amoadmopubdemo/InterstitialActivity.kt
@@ -1,4 +1,4 @@
-package amoad.com.amoadmopubdemo
+package jp.co.cyberagent.amoad.amoadmopubdemo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
