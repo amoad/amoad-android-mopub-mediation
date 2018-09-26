4f2e1b5ebcbe367e64a3f9c4e49c709f449635dd
菅原 清吾 <sugawara_seigo@cyberagent.co.jp>
2018-09-26 21:36:57 +0900

    bump

--- AMoAdMoPubDemo/app/src/main/java/jp/co/cyberagent/amoad/amoadmopubdemo/MainActivity.kt
+++ AMoAdMoPubDemo/app/src/main/java/jp/co/cyberagent/amoad/amoadmopubdemo/MainActivity.kt
@@ -4,8 +4,6 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
-import com.amoad.AMoAdBuildConfig
-import com.amoad.AMoAdLogger
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
